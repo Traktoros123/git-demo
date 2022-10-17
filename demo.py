caa0b55e3d64f9f9e948b7b485b871e34f7e0a8d
Otto Karius <otto@karius.com>
2022-10-17 15:28:53 +0200

    Updated demo.py
    Added main function.

--- demo.py
+++ demo.py
@@ -1,6 +1,9 @@
 #This is a simple Hello word progr
 
 name = input("Give me your name please: ")
-print(f'Hello {name}!')
+def greeting(name: str):
+    
+    print(f'Hello {name}!')
+greeting(name)
 
 
